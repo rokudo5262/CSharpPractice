f2c9c0d8b0fa295a055f1fdb6ed2efff2eaeda26
Trinh Quang Truong <rokudo5262@gmail.com>
2020-06-30 21:44:05 +0700

    String 30/6/2020

--- String/String.cs
+++ String/String.cs
@@ -21,6 +21,12 @@ namespace String
             Console.WriteLine("*05.Nhập Chuỗi.Sắp Xếp Chuỗi Theo Chiều Tăng Dần.                              *");
             Console.WriteLine("*06.Nhập Chuỗi.Sắp Xếp Chuỗi Theo Chiều Giảm Dần.                              *");
             Console.WriteLine("*07.Nhập Chuỗi.Đếm Số Nguyên Âm Và Phụ Âm.                                     *");
+            Console.WriteLine("*08.Nhập Chuỗi.Đếm số chữ cái, số chữ số, số ký tự đặc biệt.                   *");
+            Console.WriteLine("*09.Nhập Chuỗi.Chuyễn Ký Tự Thường Thành Ký Tự In Hoa.                         *");
+            Console.WriteLine("*10.Nhập Và Kiểm Tra Tài Khoản Và Mật Khẩu.                                    *");
+            Console.WriteLine("*11.Nhập Chuỗi.Tìm Phần Tử Xuất Hiện Nhiều Nhất.                               *");
+            Console.WriteLine("*12.Nhập 1 Ký Tự.Kiểm Tra Ký Tự Đó Là Chữ Hoa Hay Chữ Thường.                  *");
+            Console.WriteLine("*13.Nhập Chuỗi.Lấy chuỗi con từ chuỗi đã cho.                                  *");         
             Console.WriteLine("*00. Thoát Chương Trình.                                                       *");
             Console.WriteLine("********************************************************************************");
         }
@@ -32,11 +38,11 @@ namespace String
                 Menu();
                 Console.Write("Vui Lòng Chọn Chức Năng:");
                 Selectfuntion = Convert.ToInt32(Console.ReadLine());
-                if (Selectfuntion < 0 || Selectfuntion > 10)
+                if (Selectfuntion < 0 || Selectfuntion > 13)
                 {
                     Console.WriteLine("Nhập Sai. Vui Lòng Nhập Lại.");
                 }
-            } while (Selectfuntion < 0 || Selectfuntion > 10);
+            } while (Selectfuntion < 0 || Selectfuntion > 13);
             switch (Selectfuntion)
             {
                 case 1:
@@ -64,15 +70,31 @@ namespace String
                     List();
                     break;
                 case 7:
+                    DemNguyenAmPhuAm();
                     List();
                     break;
                 case 8:
+                    DemChuCaiChuSoVaKiTuDatBiet();
                     List();
                     break;
                 case 9:
+                    ChuyenKyTuThuongThanhKyTuHoa();
                     List();
                     break;
                 case 10:
+                    KiemTraTaiKhoanMatKhau();
+                    List();
+                    break;
+                case 11:
+                    TimPhanTuXuatHienNhieuNhat();
+                    List();
+                    break;
+                case 12:
+                    KiemTraKyTuHoaThuong();
+                    List();
+                    break;
+                case 13:
+                    LayChuoiCon();
                     List();
                     break;
                 default:
@@ -108,7 +130,6 @@ namespace String
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.Write("Bạn Vừa Nhập Chuỗi: {0}", str);
-            Console.Write("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
             int length = 0;
             while (length <= str.Length - 1)
             {
@@ -123,7 +144,6 @@ namespace String
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.Write("Bạn Vừa Nhập Chuỗi: {0}", str);
-            Console.Write("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
             int length = 0;
             int bien_dem = 1;
             /* lap toi phan cuoi cua chuoi */
@@ -160,7 +180,6 @@ namespace String
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
-            Console.WriteLine("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
             int l = str.Length;
             char ch;
             char[] array = str.ToCharArray(0, l);
@@ -190,7 +209,6 @@ namespace String
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
-            Console.WriteLine("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
             int l = str.Length;
             char ch;
             char[] array = str.ToCharArray(0, l);
@@ -220,15 +238,197 @@ namespace String
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
-            Console.WriteLine("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
+            int nguyen_am = 0;
+            int phu_am = 0;
+            int length = str.Length;
+            for (int i = 0; i < length; i++)
+            {
+
+                if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' ||
+                    str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' ||
+                    str[i] == 'O' || str[i] == 'U')
+                {
+                    nguyen_am++;
+                }
+                else
+                {
+                    if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                    {
+                        phu_am++;
+                    }
+                }
+            }
+            Console.WriteLine("Số Nguyên Âm Có Trong Chuỗi Là: {0}", nguyen_am);
+            Console.WriteLine("Số Phụ Âm Có Trong Chuỗi Là: {0}", phu_am);
+            Console.ReadKey();
         }
-        public static void PhanLoaiPhanTu()
+        public static void DemChuCaiChuSoVaKiTuDatBiet()
         {
             Console.Write("Nhập Chuỗi:");
             string str = Console.ReadLine();
             Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
+            int l = str.Length;
+            int chu_cai = 0;
+            int chu_so = 0;
+            int ky_tu_dac_biet = 0;
+            int i = 0;
+            /* kiem tra tung ky tu trong chuoi*/
+            while (i < l)
+            {
+                if ((str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+                {
+                    chu_cai++;
+                }
+                else if (str[i] >= '0' && str[i] <= '9')
+                {
+                    chu_so++;
+                }
+                else
+                {
+                    ky_tu_dac_biet++;
+                }
+                i++;
+            }
+            Console.WriteLine("Số Chữ Cái Trong Chuỗi Là: {0}", chu_cai);
+            Console.WriteLine("Số Chữ Số Trong Chuỗi Là: {0}", chu_so);
+            Console.WriteLine("Số Ký Tự Đặt Biệt Trong Chuỗi Là: {0}", ky_tu_dac_biet);
+            Console.ReadKey();
+        }
+        public static void ChuyenKyTuThuongThanhKyTuHoa()
+        {
+            Console.Write("Nhập Chuỗi:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
+            int l = str.Length;
+            char ch;
+            char[] array;
+            array = str.ToCharArray(0, l); // chuyen chuoi thanh mang ky tu.  
+            Console.WriteLine("Sau Khi Chuyễn Dổi, Chuỗi Có Dạng: ");
+            for (int i = 0; i < l; i++)
+            { 
+                ch = array[i];
+                if (Char.IsLower(ch)) // kiem tra ky tu thuong 
+                {
+                    Console.Write(Char.ToUpper(ch)); // chuyen doi chu thuong thanh chu hoa.  
+                }
+                else
+                {
+                    Console.Write(Char.ToLower(ch)); // chuyen doi chu hoa thanh chu thuong.  
+                }
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public static void KiemTraTaiKhoanMatKhau()
+        {
+            string username;
+            string password;
+            int ctr = 0;
+            do
+            {
+                Console.Write("Nhập Tài Khoản: ");
+                username = Console.ReadLine();
+                Console.Write("Nhập Mật Khẩu: ");
+                password = Console.ReadLine();
+                if (username != "truong" || password != "12345")
+                {
+                    ctr++;
+                }
+                else
+                {
+                    ctr = 1;
+                }
+            }
+            while ((username != "truong" || password != "12345") && (ctr != 3));
+            if (ctr == 3)
+            {
+                Console.WriteLine("Ban da nhap sai username va password qua 3 lan. Xin hay thu lai!");
+            }
+            else
+            { 
+                Console.WriteLine("Ban da nhap mat khau dung!");
+            }
+            Console.ReadKey();
+        }
+        public static void TimPhanTuXuatHienNhieuNhat()
+        {
+            Console.Write("Nhập Chuỗi:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
+            int l = str.Length;
+            int[] tan_suat=new int[255];
+            int ascii;
+            int i = 0;
+            for (i = 0; i < 255; i++)  //thiet lap tan suat xuat hien cua tat ca ky tu ve 0  
+            {
+                tan_suat[i] = 0;
+            }
+            /* Doc tan suat cua moi ky tu */
+            i = 0;
+            while (i < l)
+            {
+                ascii = (int)str[i];
+                tan_suat[ascii] += 1;
+                i++;
+            }
+            int max = 0;
+            for ( i = 0; i < 255; i++)
+            {
+                if (i != 32)
+                {
+                    if (tan_suat[i] > tan_suat[max])
+                    {
+                        max = i;
+                    }
+                }
+            }
+            Console.Write("Ký Tự Xuất Hiện Nhiều Nhất '{0}' Và Xuất Hiện {1} Lần.", (char)max, tan_suat[max]);
+            Console.ReadKey();
+        }
+        public static void KiemTraKyTuHoaThuong()
+        {
+            Console.Write("Nhập Ký Tự:");
+            string str = Console.ReadLine();
+            Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
             Console.WriteLine("In Từng Ký Tự Trong Chuổi Ban Đầu: ");
+            char ch = (char)Console.Read();
+            if (Char.IsLetter(ch))
+            {
+                if (Char.IsUpper(ch))
+                {
+                    Console.WriteLine("Ky tu vua nhap la chu hoa.");
+                }
+                else
+                {
+                    Console.WriteLine("Ky tu vua nhap la chu thuong.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Ky tu vua nhap khong phai la chu cai.");
+            }
+            Console.ReadKey();
+        }
+        public static void LayChuoiCon()
+        {
+            Console.Write("Nhập Chuỗi:");
+            string str = Console.ReadLine();        
+            Console.Write("Nhap vi tri bat dau de lay chuoi con: ");
+            int pos = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Nhap do dai cua chuoi con: ");
+            int l = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Bạn Vừa Nhập Chuỗi: {0}", str);
+            Console.Write("Chuoi con thu duoc tu chuoi ban dau la: ");
+            int ln = str.Length;
+            char[] array = str.ToCharArray(0, ln);
+            int c = 0;
+            while (c < l)
+            {
+                Console.Write(array[pos + c - 1]);
+                c++;
+            }
+            Console.WriteLine();
+            Console.ReadKey();
         }
     }
-
 }
